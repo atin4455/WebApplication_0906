e92b73581048bd1d463cc97c797a84679fe261d0
atin4455 <atinzulin@hotmail.com>
2024-09-06 10:19:03 +0800

    加入專案檔案。

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using System.Web.UI.WebControls;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginVm formData)
+        {
+            if (FormsAuthentication.Authenticate(formData.Account, formData.Password))
+            {
+                FormsAuthentication.RedirectFromLoginPage(formData.Account, false);
+
+                return null;
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "帳號或密碼有誤");
+                return View();
+            }
+
+        }
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Models/ViewModels/LoginVm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class LoginVm
+    {
+        [Required]
+        public string Account { get; set; }
+
+        public string Password { get; set; }
+    }
+}
